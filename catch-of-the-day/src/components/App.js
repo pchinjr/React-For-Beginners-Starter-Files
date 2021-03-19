3fde06bdae6ed1019acb6a36689ba4cd5f698756
Paul Chin Jr <pchinjr@gmail.com>
2021-03-19 02:41:14 +0000

    seafood market

--- catch-of-the-day/src/components/App.js
+++ catch-of-the-day/src/components/App.js
@@ -8,7 +8,7 @@ class App extends React.Component {
     return (
      <div className="catch-of-the-day">
        <div className="menu">
-         <Header tagline="Fresh Market"/>
+         <Header tagline="Fresh Seafood Market"/>
        </div>
        <Order />
        <Inventory />
